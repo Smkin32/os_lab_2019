ddaae70d8cc27dbe3ef9a673955bd91a212fa447
Smkin32 <adiksem2005@gmail.com>
2025-09-16 15:17:15 +0300

    Выполнена лабораторная работа 2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,16 @@
 #include "revert_string.h"
-
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 void RevertString(char *str)
 {
-	// your code here
+	char* newstr = (char*)malloc(sizeof(char) * (strlen(str) + 1));
+	for (int i = 0; i < strlen(str); i++)
+	{
+	    newstr[i] = str[strlen(str) - 1 - i];
+	}
+	newstr[strlen(str)] = 0;
+	strcpy(str, newstr);
+	free(newstr);
 }
 
--- lab2/src/swap/swap.c
+++ lab2/src/swap/swap.c
@@ -2,5 +2,8 @@
 
 void Swap(char *left, char *right)
 {
-	// ваш код здесь
+	char buf = *left;
+	*left = *right;
+	*right = buf;
 }
+
